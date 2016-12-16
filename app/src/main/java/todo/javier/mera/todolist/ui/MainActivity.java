df905c6be59de2385b9a83237a06e6e4f6d12fa8
javie <javier_mera88@hotmail.com>
2016-12-16 10:29:34 -0600

    Removed navigation drawer implementation, and replaced it with just showing the back button when the user is viewing a specific to-do list.

--- app/src/main/java/todo/javier/mera/todolist/ui/MainActivity.java
+++ app/src/main/java/todo/javier/mera/todolist/ui/MainActivity.java
@@ -27,15 +27,8 @@ public class MainActivity extends AppCompatActivity
     NavigationView.OnNavigationItemSelectedListener{
 
     private FragmentHelper mFragmentHelper;
-    private ActionBarDrawerToggle mToggle;
 
     @BindView(R.id.toolbar) Toolbar mToolBar;
-
-    @BindView(R.id.navigationView)
-    NavigationView mNavigationView;
-
-    @BindView(R.id.drawerLayout)
-    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,48 +43,26 @@ public class MainActivity extends AppCompatActivity
         mToolBar.setTitle("");
 
         setSupportActionBar(mToolBar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        mToggle = new ActionBarDrawerToggle(this,
-            mDrawerLayout,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        );
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
         Fragment fragment = FragmentTodoList.newInstance();
         mFragmentHelper.replace(R.id.fragmentContainer, fragment);
     }
 
     @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        mToggle.syncState();
-    }
-
-    @Override
     public void onBackPressed() {
 
-        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+        FragmentRecycler fragment = (FragmentRecycler) mFragmentHelper.findFragment("fragment_recycler");
+        if (fragment.isRemovingItems()) {
 
-            mDrawerLayout.closeDrawer(GravityCompat.START);
+            fragment.clearRemovableItems();
+            updateToolbarBackground(R.color.colorPrimary);
+            return;
         }
         else {
 
-            FragmentRecycler fragment = (FragmentRecycler) mFragmentHelper.findFragment("fragment_recycler");
-            if (fragment.isRemovingItems()) {
-
-                fragment.clearRemovableItems();
-                updateToolbarBackground(R.color.colorPrimary);
-                return;
-            }
-            else {
-
-                // If the user presses back while being on a to-do list's task fragment, then display
-                // hamburger button instead of back button.
-                mToggle.setDrawerIndicatorEnabled(true);
-                super.onBackPressed();
-            }
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            super.onBackPressed();
         }
     }
 
@@ -102,23 +73,8 @@ public class MainActivity extends AppCompatActivity
 
             case android.R.id.home:
 
-                // Check if the hamburger button is enabled when the user presses the bar button
-                // on the top left
-                if(mToggle.isDrawerIndicatorEnabled()) {
-
-                    // If the hamburger button is displayed, then open the drawer, meaning the user
-                    // is currently in the fragment that displays all to-do lists
-                    mDrawerLayout.openDrawer(GravityCompat.START);
-                }
-                else {
-
-                    // If the back button is shown instead of the hamburger button, it means the user
-                    // is currently viewing the a to-do lists's tasks so make it go back by calling
-                    // the back pressed default logic.
-                    onBackPressed();
-                }
-
-                break;
+                onBackPressed();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -133,9 +89,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void showFragmentTodoList(TodoList todoList) {
 
-        // If the user has selected a to-do list, disable the hamburger button in order to display
-        // the back button.
-        mToggle.setDrawerIndicatorEnabled(false);
+        // When the user selects a list, disply the back button also on the top left of the toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Fragment fragment = FragmentTask.newInstance(todoList);
         mFragmentHelper.replaceWithBackStack(
